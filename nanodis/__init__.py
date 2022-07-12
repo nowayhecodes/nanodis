ddd0b6d69da4e720b096f3f435e253420aeaea17
Gustavo Cavalcante <nowayhecodes@gmail.com>
2022-07-12 00:04:43 -0300

    feat: added hash fns

--- nanodis/__init__.py
+++ nanodis/__init__.py
@@ -25,11 +25,8 @@ import logging
 import optparse
 import os
 import pickle
-from symtable import Symbol
 import sys
 import time
-
-from numpy import isin
 
 
 from .exceptions import (ClientQuit, Shutdown,
@@ -677,3 +674,73 @@ class QueueServer(object):
             return datetime.datetime.strptime(timestamp, fmt)
         except ValueError:
             raise CmdError('Timestamp must be formatted Y-m-d H:M:S')
+
+    @enforce_datatype(HASH)
+    def hdel(self, key, field):
+        value = self._kv[key].value
+        if field in value:
+            del value[field]
+            return 1
+        return 0
+
+    @enforce_datatype(HASH)
+    def hexists(self, key, field):
+        value = self._kv[key].value
+        return 1 if field in value else 0
+
+    @enforce_datatype(HASH)
+    def hget(self, key, field):
+        return self._kv[key].value.get(field)
+
+    @enforce_datatype(HASH)
+    def hgetall(self, key):
+        return self._kv[key].value
+
+    @enforce_datatype(HASH)
+    def hincrby(self, key, field, incr=1):
+        self._kv[key].value.setdefault(field, 0)
+        self._kv[key].value[field] += incr
+        return self._kv[key].value[field]
+
+    @enforce_datatype(HASH)
+    def hkeys(self, key):
+        return list(self._kv[key].value)
+
+    @enforce_datatype(HASH)
+    def hlen(self, key):
+        return len(self._kv[key].value)
+
+    @enforce_datatype(HASH)
+    def hmget(self, key, *fields):
+        accum = {}
+        value = self._kv[key].value
+        for field in fields:
+            accum[field] = value.get(field)
+        return accum
+
+    @enforce_datatype(HASH)
+    def hmset(self, key, data):
+        self._kv[key].value.update(data)
+        return len(data)
+
+    @enforce_datatype(HASH)
+    def hset(self, key, field, value):
+        self._kv[key].value[field] = value
+        return 1
+
+    @enforce_datatype(HASH)
+    def hsetnx(self, key, field, value):
+        kval = self._kv[key].value
+        if field not in kval:
+            kval[field] = value
+            return 1
+        return 0
+
+    @enforce_datatype(HASH)
+    def hvals(self, key):
+        return list(self._kv[key].value.values())
+
+    @enforce_datatype(SET)
+    def sadd(self, key, *members):
+        self._kv[key].value.update(members)
+        return len(self._kv[key].value)
