0de56528190d820806f9c4924d9f57d660a50914
Gustavo Cavalcante <nowayhecodes@gmail.com>
2022-07-13 16:42:44 -0300

    feat: SocketPool

--- nanodis/__init__.py
+++ nanodis/__init__.py
@@ -952,3 +952,69 @@ class QueueServer(object):
             logger.debug(f'received {decode(cmd)}')
 
         return self._commands[cmd](*data[1:])
+
+
+@njit
+class SocketPool(object):
+    def __init__(self, host, port, max_age=60) -> None:
+        self.host = host
+        self.port = port
+        self.max_age = max_age
+        self.free = []
+        self.in_use = {}
+        self._tid = get_ident if HAVE_GEVENT else get_ident_threaded
+
+    def checkout(self):
+        now = time.time()
+        tid = self._tid()
+
+        if tid in self.in_use:
+            sock = self.in_use[tid]
+
+            if sock.closed:
+                del self.in_use[tid]
+            else:
+                return self.in_use[tid]
+
+        while self.free:
+            ts, sock = heapq.heappop(self.free)
+            if ts < now - self.max_age:
+                try:
+                    sock.close()
+                except OSError:
+                    pass
+
+            else:
+                self.in_use[tid] = sock
+                return sock
+
+        sock = self.create_socket_file()
+        self.in_use[tid] = sock
+        return sock
+
+    def create_socket_file(self):
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        conn.connect((self.host, self.port))
+        return conn.makefile('rwb')
+
+    def checkin(self):
+        tid = self._tid()
+        if tid in self.in_use:
+            sock = self.in_use.pop(tid)
+            if not sock.closed:
+                heapq.heappush(self.free, (time.time(), sock))
+            return True
+        return False
+
+    def close(self):
+        tid = self._tid()
+        sock = self.in_use.pop(tid, None)
+
+        if sock:
+            try:
+                sock.close()
+            except OSError:
+                pass
+            return True
+        return False
